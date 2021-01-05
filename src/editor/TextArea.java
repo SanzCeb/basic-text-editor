d1313119a7ebc892d27053d986edd9185e3546d0
Sergio Sanz Cebollero <scebollero@guidewire.com>
2021-01-05 17:47:17 +0100

    Stage 1/4: A window to editing

---
+++ src/editor/TextArea.java
@@ -0,0 +1,11 @@
+package editor;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextArea extends JTextArea {
+    public TextArea() {
+        setSize(new Dimension(200, 200));
+        setName("TextArea");
+    }
+}
--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -3,10 +3,16 @@ package editor;
 import javax.swing.*;
 
 public class TextEditor extends JFrame {
+    private JTextArea textArea;
     public TextEditor() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
-        setVisible(true);
+        setTitle("Text Editor");
         setLayout(null);
+        textArea = new TextArea();
+        add(textArea);
+        textArea.setLocation(25, 25);
+        setVisible(true);
+
     }
 }
